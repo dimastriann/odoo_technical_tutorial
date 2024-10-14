4a292de9ad8fd9c591efc34c5ca9a05179c35585
dimastriann <dimastriannugraha@gmail.com>
2024-10-14 22:23:24 +0700

    [ADD] owl new component

--- owl_framework_tutorial/static/src/app/new_app.js
+++ owl_framework_tutorial/static/src/app/new_app.js
@@ -1,7 +1,7 @@
 /** @odoo-module **/
 
 import { templates} from "@web/core/assets";
-import { Component, App, whenReady } from "@odoo/owl";
+import { Component, App, whenReady, xml } from "@odoo/owl";
 import {makeEnv, startServices} from "@web/env";
 
 export class MenuComponent extends Component {}
@@ -10,9 +10,27 @@ MenuComponent.template = "MenuComponent";
 export class MenuComponent2 extends Component {}
 MenuComponent2.template = "MenuComponent2";
 
+
+export class MyFirstComponent extends Component {
+
+    static template = "MyFirstComponent";
+    setup(){
+        super.setup()
+        this.my_prop = false;
+    }
+
+    click(){
+        console.log(this)
+    }
+}
+// MyFirstComponent.template = "";
+
+
 export class MyRootComponent extends Component {}
 MyRootComponent.template = "MyRootComponent";
-MyRootComponent.components = { MenuComponent, MenuComponent2 };
+MyRootComponent.components = {
+    MenuComponent, MenuComponent2, MyFirstComponent
+};
 //
 (async function startNewApp() {
     const env = makeEnv()
